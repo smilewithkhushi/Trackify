4d3a5c612ed428d789e50d4f73231fd58ce44e6e
smilewithkhushi <khushipanwar26@gmail.com>
2024-03-28 10:32:58 +0530

    setup radix -ui

---
+++ app/api/issues/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssuesSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1).max(255),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validation = createIssuesSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+
+    const newIssue = await prisma.issue.create({
+        data : { title: body.title, description: body.description }
+    })
+
+    return NextResponse.json(newIssue, { status: 201 })
+
+}
+
+//zod is used for data validation
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const page = () => {
   return (
-    <div>page</div>
+    <div><Button> New Issue </Button></div>
   )
 }
 
