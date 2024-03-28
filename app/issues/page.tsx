c507d3765d5c5c14d378abf07f0a667b5c277d45
smilewithkhushi <khushipanwar26@gmail.com>
2024-03-28 10:50:33 +0530

    customize radix ui theme

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Button } from '@radix-ui/themes';
-import Link from 'next/link';
+import React from "react";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const page = () => {
   return (
-    <div><Button>
-        <Link href='/issues/new'> New Issue </Link>  </Button></div>
-  )
-}
+    <div>
+      <Button>
+        <Link href="/issues/new"> New Issue </Link>{" "}
+      </Button>
+    </div>
+  );
+};
 
-export default page
+export default page;
